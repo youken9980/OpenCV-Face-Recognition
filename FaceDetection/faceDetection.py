8de5220d335c924b7bbf9c3c89a6927a1d6604c6
youken9980 <youken9980@163.com>
2018-11-20 14:53:56 +0800

    修正摄像头成像颠倒的问题

--- FaceDetection/faceDetection.py
+++ FaceDetection/faceDetection.py
@@ -1,8 +1,8 @@
 '''
-Haar Cascade Face detection with OpenCV  
+Haar Cascade Face detection with OpenCV
     Based on tutorial by pythonprogramming.net
-    Visit original post: https://pythonprogramming.net/haar-cascade-face-eye-detection-python-opencv-tutorial/  
-Adapted by Marcelo Rovai - MJRoBot.org @ 7Feb2018 
+    Visit original post: https://pythonprogramming.net/haar-cascade-face-eye-detection-python-opencv-tutorial/
+Adapted by Marcelo Rovai - MJRoBot.org @ 7Feb2018
 '''
 
 import numpy as np
@@ -17,14 +17,12 @@ cap.set(4,480) # set Height
 
 while True:
     ret, img = cap.read()
-    img = cv2.flip(img, -1)
+    img = cv2.flip(img, 1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(
         gray,
-        
         scaleFactor=1.2,
-        minNeighbors=5
-        ,     
+        minNeighbors=5,
         minSize=(20, 20)
     )
 
@@ -32,7 +30,6 @@ while True:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        
 
     cv2.imshow('video',img)
 
