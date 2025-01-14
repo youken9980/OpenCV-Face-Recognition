414fd3773a778aabf2a03c8fd851f0a28e3814d9
youken9980 <youken9980@163.com>
2025-01-14 13:45:05 +0800

    更新脚本

--- FaceDetection/faceSmileDetection.py
+++ FaceDetection/faceSmileDetection.py
@@ -14,8 +14,8 @@ faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xm
 smileCascade = cv2.CascadeClassifier('Cascades/haarcascade_smile.xml')
 
 cap = cv2.VideoCapture(0)
-cap.set(3,640) # set Width
-cap.set(4,480) # set Height
+cap.set(3,960) # set Width
+cap.set(4,720) # set Height
 
 while True:
     ret, img = cap.read()
--- FaceDetection/faceSmileEyeDetection.py
+++ FaceDetection/faceSmileEyeDetection.py
@@ -15,8 +15,8 @@ eyeCascade = cv2.CascadeClassifier('Cascades/haarcascade_eye.xml')
 smileCascade = cv2.CascadeClassifier('Cascades/haarcascade_smile.xml')
 
 cap = cv2.VideoCapture(0)
-cap.set(3,640) # set Width
-cap.set(4,480) # set Height
+cap.set(3,960) # set Width
+cap.set(4,720) # set Height
 
 while True:
     ret, img = cap.read()
